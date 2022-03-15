0009761be1c62a493f5c29ab02220c9d278f9654
Name <maximovyaa@gmail.com>
2022-03-15 23:07:03 +0300

    '1'

--- new_portal/news_portal/management/commands/runapscheduler.py
+++ new_portal/news_portal/management/commands/runapscheduler.py
@@ -1,4 +1,12 @@
 import logging
+from _multiprocessing import send
+
+from collections import defaultdict
+from datetime import datetime, timedelta, tzinfo
+
+from django.template.loader import render_to_string
+from django.utils import timezone
+from django.core.mail import EmailMultiAlternatives
 
 from django.conf import settings
 
@@ -8,14 +16,54 @@ from django.core.management.base import BaseCommand
 from django_apscheduler.jobstores import DjangoJobStore
 from django_apscheduler.models import DjangoJobExecution
 
-logger = logging.getLogger(__name__)
+from news_portal.models import Post
 
+logger = logging.getLogger(__name__)
+def send_posts(email_list, posts):
+    # на случай, если там только один адрес, а не список
+    if isinstance(email_list, str):
+        subscriber_list = [email_list, ]
+    else:
+        subscriber_list = email_list
+
+    email_form = settings.DEFAULT_FROM_EMAIL
+    subject = 'В категориях, на которые вы подписаны появились новые статьи'
+    text_message = 'В категориях, на которые вы подписаны появились новые статьи'
+
+    # рендерим в строку шаблон письма и передаём туда переменные, которые в нём используем
+    render_html_template = render_to_string('send_posts_list.html', {'posts': posts, 'subject': subject})
+
+    # формируем письмо
+    msg = EmailMultiAlternatives(subject, text_message, email_form, list(subscriber_list))
+
+    # прикрепляем html-шаблон
+    msg.attach_alternative(render_html_template, 'text/html')
+    # отправляем
+    msg.send()
 
 # наша задача по выводу текста на экран
-def my_job():
-    #  Your job processing logic here...
-    print('hello from job')
+def send_posts_to_email_weekly():
+    """
+    таск который выбирает все посты, опубликованные за неделю и рассылающий
+    (через вызов вспомогательной функции) их всем, кто подписан на категории, куда эти статьи входят
+    :return: None
+    """
+    # берём посты за последние 7 дней
+    # здесь мы получаем queryset
+    last_week_posts_qs = Post.objects.filter(datetime.now(+3)-timedelta(days=7))
 
+
+    # собираем в словарь список пользователей и список постов, которые им надо разослать
+    posts_for_user = defaultdict(set)  # user -> posts
+
+    for post in last_week_posts_qs:
+        for category in post.postCategory.all():
+            for user in category.subscribers.all():
+                posts_for_user[user].add(post)
+
+    # непосредственно рассылка
+    for user, posts in posts_for_user.items():
+        send_posts(user.email, posts)
 
 # функция, которая будет удалять неактуальные задачи
 def delete_old_job_executions(max_age=604_800):
@@ -32,14 +80,14 @@ class Command(BaseCommand):
 
         # добавляем работу нашему задачнику
         scheduler.add_job(
-            my_job,
-            trigger=CronTrigger(second="*/1"),
+            send_posts_to_email_weekly,
+            trigger=CronTrigger(second="*/5"),
             # То же, что и интервал, но задача тригера таким образом более понятна django
-            id="my_job",  # уникальный айди
+            id="send_posts_to_email_weekly",  # уникальный айди
             max_instances=1,
             replace_existing=True,
         )
-        logger.info("Added job 'my_job'.")
+        logger.info("Added job 'send_posts_to_email_weekly'.")
 
         scheduler.add_job(
             delete_old_job_executions,
